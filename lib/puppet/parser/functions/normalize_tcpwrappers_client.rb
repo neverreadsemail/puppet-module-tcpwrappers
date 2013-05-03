e37d1c19f4d2cd143368aa28e4f542dc3c40485e
Jon C. Meyer <jmeyer@ucsd.edu>
2013-05-03 15:28:55 -0700

    Add split/rejoin on space logic to lib for batch normalization.

--- lib/puppet/parser/functions/normalize_tcpwrappers_client.rb
+++ lib/puppet/parser/functions/normalize_tcpwrappers_client.rb
@@ -8,47 +8,52 @@ module Puppet::Parser::Functions
         args.length == 1 or
         raise Puppet::Error.new("#{__method__}: expecting 1 argument")
 
-        client = args[0]
-        retval = client
+        # iterate over the string after we split on space
+        retarr = []
+        args[0].split(' ').each do |client|
+            v = client
 
-        if not client.is_a? String
-            raise Puppet::Error.new("#{__method__}: argument must be a String")
-        end
+            if not client.is_a? String
+                raise Puppet::Error.new(
+                    "#{__method__}: argument must be a String")
+            end
 
-        # Convert to IPAddr if we can.
-        begin
-            ip      = IPAddr.new(client)
+            # Convert to IPAddr if we can.
+            begin
+                ip      = IPAddr.new(client)
 
-            # Return IPv6 as we got it, process IPv4.
-            if ip.ipv4?
-                masklen = client.split('/')[1] || 32
-                netmask = IPAddr.new("255.255.255.255").mask(masklen)
+                # Return IPv6 as we got it, process IPv4.
+                if ip.ipv4?
+                    masklen = client.split('/')[1] || 32
+                    netmask = IPAddr.new("255.255.255.255").mask(masklen)
 
-                case netmask.to_i
-                when 4278190080 # /8
-                    retval = ip.to_s.split('.').slice(0,1).join('.')+'.'
-                when 4294901760 # /16
-                    retval = ip.to_s.split('.').slice(0,2).join('.')+'.'
-                when 4294967040 # /24
-                    retval = ip.to_s.split('.').slice(0,3).join('.')+'.'
-                when 4294967295 # /32
-                    retval = ip.to_s
-                else # Some other valid IPv4 IP/netmask.
-                    retval = "#{ip.to_s}/#{netmask.to_s}"
+                    case netmask.to_i
+                    when 4278190080 # /8
+                        v = ip.to_s.split('.').slice(0,1).join('.')+'.'
+                    when 4294901760 # /16
+                        v = ip.to_s.split('.').slice(0,2).join('.')+'.'
+                    when 4294967040 # /24
+                        v = ip.to_s.split('.').slice(0,3).join('.')+'.'
+                    when 4294967295 # /32
+                        v = ip.to_s
+                    else # Some other valid IPv4 IP/netmask.
+                        v = "#{ip.to_s}/#{netmask.to_s}"
+                    end
+                end
+
+            rescue ArgumentError => e
+                # if client is like Hostname,FQDN,suffix,filename,keyword,etc.
+                case client
+                when 'ALL','LOCAL','PARANOID',/^\.?[a-z\d_.]+$/,/^\/[^ \n\t,:#]+$/
+                    # NOOP
+                else
+                    raise Puppet::Error.new(
+                        "#{__method__}: invalid spec: #{client}, #{e}")
                 end
             end
-
-        rescue ArgumentError => e
-            # if client is like Hostname, FQDN, suffix, filename, keyword, etc.
-            case client
-            when 'ALL','LOCAL','PARANOID',/^\.?[a-z\d_.]+$/,/^\/[^ \n\t,:#]+$/
-                # NOOP
-            else
-                raise Puppet::Error.new(
-                    "#{__method__}: invalid spec: #{client}, #{e}")
-            end
+            retarr.push(v)
         end
-
-        retval
+        # Join on space before we return
+        retarr.join(' ')
     end
 end
